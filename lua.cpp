dc64b3f9558d0ff9f83e3e760b50fc44977412a8
Trey M <treym2k.dev@gmail.com>
2021-03-27 07:53:07 -0400

    Added basic GDScript function exposure to lua

--- lua.cpp
+++ lua.cpp
@@ -1,41 +1,274 @@
 
 #include "lua.h"
 
-#include <string>
+Lua::Lua(){
+  state = luaL_newstate();
+  luaL_openlibs(state);
 
-Lua::Lua(){}
+  lua_register(state, "print", luaPrint);
+}
+
+Lua::~Lua(){
+  lua_close(state);
+}
 
 void Lua::_bind_methods(){
   ClassDB::bind_method(D_METHOD("run", "code"), &Lua::run);
+  ClassDB::bind_method("execute", &Lua::execute);
+  ClassDB::bind_method("load",&Lua::load);
+  ClassDB::bind_method("pushVariant",&Lua::pushGlobalVariant);
+  ClassDB::bind_method(D_METHOD("exposeFunction", "NodeObject", "GDFunction", "LuaFunctionName"),&Lua::exposeFunction);
 }
 
-int luaPrint(lua_State* state)
-{
+void Lua::exposeFunction(Object *instance, String function, String name){
+  
+  auto f = [](lua_State* state) -> int{
+    const Object *instance2 = (const Object*) lua_topointer(state, lua_upvalueindex(1));
+    const char *function2 = (const char*) lua_tostring(state, lua_upvalueindex(2));
+    ScriptInstance *scriptInstance = instance2->get_script_instance();
+    scriptInstance->call(function2);
 
-  int args = lua_gettop(state);
+    return 0;
+  };
 
-  for ( int n=1; n<=args; ++n) {
-    print_line(lua_tostring(state, n));
-  }
+  lua_pushlightuserdata(state, instance);
+  std::wstring temp = function.c_str();
+  const char *func = std::string ( temp.begin(), temp.end() ).c_str();
 
-  return 0;
+  std::wstring ntemp = name.c_str();
+  const char *fname = std::string ( ntemp.begin(), ntemp.end() ).c_str();
+
+  lua_pushstring(state, func);
+  lua_pushcclosure(state, f, 2);
+  lua_setglobal(state, fname);
+  
+}
+
+bool Lua::load(String fileName) {
+    _File file;
+
+    file.open(fileName,_File::ModeFlags::READ);
+
+    String text = file.get_as_text();
+    Vector<String> lines = text.split("\n");
+    std::map<String,String> allFunctions;
+    String inFunction = "";
+    String functionBegin = "function";
+    String functionEnd = "end";
+
+    for (int i = 0; i < lines.size(); i++) {
+        String line = lines[i];
+        if (inFunction == "") {
+            if (line.begins_with(functionBegin)) {
+
+                int endPos = line.find("(");
+                String name = line.substr(functionBegin.length(),endPos-functionBegin.length()).strip_edges();               
+                allFunctions.insert(std::pair<String,String>(name,line+"\n"));
+                inFunction = name;
+            }
+        } else {
+            allFunctions[inFunction]+=(line+"\n");
+            if (line == functionEnd) {
+                inFunction = "";
+            }
+        }
+    }
+
+    for(std::pair<String,String> function : allFunctions) {
+      std::wstring luaCode = function.second.c_str();
+
+        if (luaL_loadstring(state, std::string( luaCode.begin(), luaCode.end() ).c_str()) || (lua_pcall(state, 0, 0 , 0))) {
+            print_line("Error: script not loaded (" + function.first + ")");
+            state = 0;
+            return false;
+        }
+    }
+    file.close();
+    return true;
+}
+
+Variant Lua::execute(String name, Array array) {
+    std::wstring luaCode =  name.c_str();
+    lua_getglobal(state, std::string( luaCode.begin(), luaCode.end() ).c_str());
+    for (int i = 0; i < array.size(); i++) {
+        Variant var = array[i];
+        pushVariant(var);
+    }
+
+    lua_pcall(state, array.size(), LUA_MULTRET, 0);
+    int numReturns = lua_gettop(state);
+
+    if (numReturns) {
+        Array results;
+        for(int i = 0; i < numReturns; i++) {
+            results.append(popVariant());
+        }
+        return results;
+    } else {
+        return Variant();
+    }
+}
+
+bool Lua::pushVariant(Variant var) {
+    std::wstring str;
+    switch (var.get_type())
+    {
+        case Variant::Type::STRING:
+            str = (var.operator String().c_str());
+            lua_pushstring(state, std::string( str.begin(), str.end() ).c_str());
+            break;
+        case Variant::Type::INT:
+            lua_pushinteger(state, (int64_t)var);
+            break;
+        case Variant::Type::REAL:
+            lua_pushnumber(state,var.operator double());
+            break;
+        case Variant::Type::BOOL:
+            lua_pushboolean(state, (bool)var);
+            break;
+        case Variant::Type::POOL_BYTE_ARRAY:
+        case Variant::Type::POOL_INT_ARRAY:
+        case Variant::Type::POOL_STRING_ARRAY:
+        case Variant::Type::POOL_REAL_ARRAY:
+        case Variant::Type::POOL_VECTOR2_ARRAY:
+        case Variant::Type::POOL_VECTOR3_ARRAY:
+        case Variant::Type::POOL_COLOR_ARRAY:            
+        case Variant::Type::ARRAY: {
+            Array array = var.operator Array();
+            lua_newtable(state);
+            for(int i = 0; i < array.size(); i++) {
+                Variant key = i+1;
+                Variant value = array[i];
+                pushVariant(key);
+                pushVariant(value);
+                lua_settable(state,-3);
+            }
+            break;
+        }
+        case Variant::Type::DICTIONARY:
+            lua_newtable(state);
+            for(int i = 0; i < ((Dictionary)var).size(); i++) {
+                Variant key = ((Dictionary)var).keys()[i];
+                Variant value = ((Dictionary)var)[key];
+                pushVariant(key);
+                pushVariant(value);
+                lua_settable(state, -3);
+            }
+            break;
+        case Variant::Type::VECTOR2: {
+            Vector2 vector2 = var.operator Vector2();
+            lua_newtable(state);
+            lua_pushstring(state, "X");
+            lua_pushnumber(state, vector2.x);
+            lua_settable(state, -3);
+            lua_pushstring(state, "Y");
+            lua_pushnumber(state, vector2.y);
+            lua_settable(state, -3);
+            break;     
+        }     
+        case Variant::Type::VECTOR3: {
+            Vector3 vector3 = var.operator Vector3();
+            lua_newtable(state);
+            lua_pushstring(state,"X");
+            lua_pushnumber(state,vector3.x);
+            lua_settable(state,-3);
+            lua_pushstring(state,"Y");
+            lua_pushnumber(state,vector3.y);
+            lua_settable(state,-3);
+            lua_pushstring(state,"Z");
+            lua_pushnumber(state,vector3.z);
+            lua_settable(state,-3);
+            break; 
+        }
+        case Variant::Type::COLOR: {
+            Color color = var.operator Color();
+            lua_newtable(state);
+            lua_pushstring(state,"R");
+            lua_pushnumber(state,color.r);
+            lua_settable(state,-3);
+            lua_pushstring(state,"G");
+            lua_pushnumber(state,color.g);
+            lua_settable(state,-3);
+            lua_pushstring(state,"B");
+            lua_pushnumber(state,color.b);
+            lua_settable(state,-3);
+            lua_pushstring(state,"A");
+            lua_pushnumber(state,color.a);
+            lua_settable(state,-3);             
+            break;
+        }
+        default:
+            print_line(var);
+            return false;
+    }
+    return true;
+}
+
+bool Lua::pushGlobalVariant(Variant var, String name) {
+    if (pushVariant(var)) {
+       std::wstring str = name.c_str();
+        lua_setglobal(state,std::string( str.begin(), str.end() ).c_str());
+        return true;
+    }
+    return false;
+}
+
+Variant Lua::popVariant() {
+    Variant result = getVariant();
+    lua_pop(state, 1);
+    return result;
+}
+
+Variant Lua::getVariant(int index) {
+    Variant result;
+    int type = lua_type(state, index);
+    switch (type) {
+        case LUA_TSTRING:
+            result = lua_tostring(state, index);
+            break;
+        case LUA_TNUMBER:
+            result = lua_tonumber(state, index);
+            break;
+        case LUA_TBOOLEAN:
+            result = (bool)lua_toboolean(state, index);
+            break;
+        case LUA_TTABLE:
+        {
+            Dictionary dict;
+            for (lua_pushnil(state); lua_next(state, index-1); lua_pop(state, 1)) {
+                Variant key = getVariant(-2);
+                Variant value = getVariant(-1);
+                dict[key] = value;
+            }
+            if (dict.size() == 2) {
+                if (dict.keys().count("X") && dict.keys().count("Y")) {
+                    result = Vector2(dict["X"],dict["Y"]);
+                }
+            } else if (dict.size() == 3) {
+                if (dict.keys().count("X") && dict.keys().count("Y") && dict.keys().count("Z")) {
+                    result = Vector3(dict["X"],dict["Y"],dict["Z"]);
+                }
+            } else if (dict.size() == 4) {
+                if (dict.keys().count("R") && dict.keys().count("G") && dict.keys().count("B") && dict.keys().count("B")) {
+                    result = Color(dict["R"],dict["G"],dict["B"],dict["A"]);
+                }
+            } else {
+                result = dict;
+            }
+            break;
+        }
+        default:
+            result = Variant();
+    }
+    return result;
 }
 
 void Lua::run(String code) {
 
-
-  lua_State *state = luaL_newstate();
-
-  luaL_openlibs(state);
-
-  lua_register(state, "print", luaPrint);
-
   int result;
 
   std::wstring luaCode = code.c_str();
-  const std::string luaCode2( luaCode.begin(), luaCode.end() );
-
-  result = luaL_loadstring(state, luaCode2.c_str());
+  result = luaL_loadstring(state, std::string( luaCode.begin(), luaCode.end() ).c_str());
 
   if ( result != LUA_OK ) {
     print_line("Rip1!");
@@ -50,3 +283,17 @@ void Lua::run(String code) {
   }
 }
 
+// Lua functions
+
+// Change lua's print function to print to the Godot console by default
+int Lua::luaPrint(lua_State* state)
+{
+
+  int args = lua_gettop(state);
+
+  for ( int n=1; n<=args; ++n) {
+    print_line(lua_tostring(state, n));
+  }
+
+  return 0;
+}
--- lua.h
+++ lua.h
@@ -3,8 +3,11 @@
 #define CALCULATORREF_H
 
 #include "core/reference.h"
+#include "core/bind/core_bind.h"
 
 #include <lua.hpp>
+#include <map>
+#include <string>
 
 class Lua : public Reference {
   GDCLASS(Lua, Reference);
@@ -14,7 +17,24 @@ protected:
 
 public:
   Lua();
+  ~Lua();
+  
   void run(String code);
+  void exposeFunction(Object *p_instance, String function, String name);
+
+  bool load(String fileName);
+  bool pushVariant(Variant var);
+  bool pushGlobalVariant(Variant var, String name);
+  Variant execute(String name, Array array);
+  Variant popVariant();
+  Variant getVariant(int index = -1);
+  
+
+  // Lua functions
+  static int luaPrint(lua_State* state);
+
+private:
+  lua_State *state;
 };
 
 #endif
