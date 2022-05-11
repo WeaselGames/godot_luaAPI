897cd9251ceec17b6e78ba76a6be3089b0aa7bbb
Trey M <treym2k.dev@gmail.com>
2022-05-11 14:58:20 -0500

    typo fix

--- luaError.cpp
+++ luaError.cpp
@@ -55,10 +55,10 @@ LuaError::ErrorType LuaError::getType() {
 }
 
 void LuaError::_bind_methods(){
-    ClassDB::bind_static_method("LuaError", D_METHOD("new_err", "Message", "Type"), &LuaError::newError);
+    ClassDB::bind_static_method("LuaError", D_METHOD("new_error", "Message", "Type"), &LuaError::newError);
     ClassDB::bind_static_method("LuaError", D_METHOD("error_none"), &LuaError::errNone);
     ClassDB::bind_static_method("LuaError", D_METHOD("is_error", "var"), &LuaError::isErr);
-
+    
     ClassDB::bind_method(D_METHOD("set_msg","Message"), &LuaError::setMsg);
     ClassDB::bind_method(D_METHOD("get_msg"), &LuaError::getMsg);
     ClassDB::bind_method(D_METHOD("set_type","Type"), &LuaError::setType);
