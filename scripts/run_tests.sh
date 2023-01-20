6dd19173f64f031f05879dbf6886db3924e90694
Trey Moller <treym2k.dev@gmail.com>
2023-01-20 11:35:16 -0600

    fixed run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-./scripts/godot/bin/godot.linuxbsd.editor.x86_64.luaAPI --headless --path ../testing -s run_tests.gd 2>&1 | tee ../report.txt
+./godot/bin/godot.linuxbsd.editor.x86_64.luaAPI --headless --path ../testing -s run_tests.gd 2>&1 | tee ../report.txt
