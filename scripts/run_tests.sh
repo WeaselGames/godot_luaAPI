ddaaba2897af477793a7bf1dcef2b27e48934ca1
Trey Moller <treym2k.dev@gmail.com>
2023-01-20 12:38:08 -0600

    fix

---
+++ scripts/run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+# we expect the working directory to be root.
+scripts/godot/bin/godot.linuxbsd.editor.x86_64.luaAPI --headless --path testing/ -s run_tests.gd
+
+ERRFILE=testing/log.txt.error
+if test -f "$ERRFILE"; then
+    exit 1
+fi
