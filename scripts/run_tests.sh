89e833efd56d894f3eb6da1f71627312c06aabfb
Trey M <treym2k.dev@gmail.com>
2023-03-26 18:12:51 -0500

    Action fixes

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 # we expect the working directory to be root.
 scripts/godot/bin/godot.linuxbsd.editor.x86_64.luaAPI --headless --path testing/ -s run_tests.gd
-mv testing/log.txt testing/log-lua.txt
-scripts/godot/bin/godot.linuxbsd.editor.x86_64.luaAPI.luaJIT --headless --path testing/ -s run_tests.gd
-mv testing/log.txt testing/log-luaJIT.txt
