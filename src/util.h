e762193ebdee3a1d1326a14e0baf33a4fb596da0
Radiant <69520693+RadiantUwU@users.noreply.github.com>
2023-06-07 16:05:22 +0300

    get_all_scripts_from_object

--- src/util.h
+++ src/util.h
@@ -3,7 +3,22 @@
 
 #ifndef LAPI_GDEXTENSION
 #include <core/string/print_string.h>
+#include <core/variant/array.h>
+
+inline Array get_all_scripts_from_object(const Object const* obj) {
+	Array a;
+	Variant script = obj.get_script();
+	while (script != nullptr) {
+		if (script.get_type() != Variant.Type.OBJECT) {
+			break; //not a script
+		}
+		a.insert(script);
+	}
+	return a;
+}
+
 #else
+#error cannot find array.h for non gdextension //TODO: fix it dammit
 #include <godot_cpp/variant/utility_functions.hpp>
 using namespace godot;
 
