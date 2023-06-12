41542e6e317544d46bf609572007896b82f80462
Radiant <69520693+RadiantUwU@users.noreply.github.com>
2023-06-12 22:29:55 +0300

    Move function

--- src/util.h
+++ src/util.h
@@ -13,19 +13,5 @@ inline void print_line(const Variant &v) {
 	UtilityFunctions::print(v);
 }
 #endif
-inline Array get_all_scripts_from_object(const Object *obj) {
-	Array a;
-	if (obj == nullptr)
-		return a;
-	Variant script = obj->get_script();
-	if (!script.is_null()) {
-		while (!script.is_null()) {
-			if (script.get_type() != Variant::Type::OBJECT) {
-				break; //not a script
-			}
-			a.append(script);
-		}
-	}
-	return a;
-}
+
 #endif
