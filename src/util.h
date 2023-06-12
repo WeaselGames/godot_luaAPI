15b9188ec711a5a01ad442dc46cb65210247baf5
Radiant <69520693+RadiantUwU@users.noreply.github.com>
2023-06-12 17:16:38 +0300

    Fix bug

--- src/util.h
+++ src/util.h
@@ -15,14 +15,16 @@ inline void print_line(const Variant &v) {
 #endif
 inline Array get_all_scripts_from_object(const Object const *obj) {
 	Array a;
-	if (obj == null)
+	if (obj == nullptr)
 		return a;
-	Variant script = obj.get_script();
-	while (script != nullptr) {
-		if (script.get_type() != Variant.Type.OBJECT) {
-			break; //not a script
+	Variant script = obj->get_script();
+	if (!script->is_null()) {
+		while (!script->is_null()) {
+			if (script.get_type() != Variant::Type.OBJECT) {
+				break; //not a script
+			}
+			a.append(script);
 		}
-		a.append(script);
 	}
 	return a;
 }
