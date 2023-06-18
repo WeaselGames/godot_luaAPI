f3cf3714ba70f1edada0417e480fabdcd07f8e93
Radiant <i.like.using.discord@gmail.com>
2023-06-18 19:34:52 +0300

    Im such a dum dum pls forgive me :sob:

--- src/classes/luaAPI.cpp
+++ src/classes/luaAPI.cpp
@@ -10,7 +10,7 @@ LuaAPI::LuaAPI() {
 	lState = luaL_newstate();
 	// Creating lua state instance
 	state.setState(lState, this, true);
-	lua_setwarnf(lState, LuaAPI::default_warnf, this);
+	state.triggerChangedWarnFunction(this);
 }
 
 LuaAPI::~LuaAPI() {
